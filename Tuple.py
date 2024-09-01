abcde915d3217f2ae33769dd12c6a79edf1d1969
Dev-Prod123 Production <nagesrajavarapu@gmail.com>
2024-09-01 05:25:53 +0000

    This is Tuple file

---
+++ Tuple.py
@@ -0,0 +1,7 @@
+#This is all about Tuple
+my_tuple = ("John",1,2,3,5)
+second_tuple = (3,4,5,6,7)
+output = my_tuple + second_tuple
+output1 = my_tuple.index("John")
+print(f"Index {my_tuple[0]} This element {output1}")
+print(output)
