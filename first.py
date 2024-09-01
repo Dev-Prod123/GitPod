6855e8d5483298d4400e8bf99aaf1da696c89933
Dev-Prod123 Production <nagesrajavarapu@gmail.com>
2024-09-01 05:09:04 +0000

    This is the List file

---
+++ first.py
@@ -0,0 +1,31 @@
+#Data stuctures in python
+#List Declaration []
+#my_list = [1,2,3,4,5,6]
+#my_list.append("john") #This is one argument
+#my_list.insert(1,"Update") #two arguments
+#print(f"Length of The list is {len(my_list)} ")
+#print(my_list)
+#output = my_list.pop() #remove the last index
+#print(f"Before {my_list}")
+#print(f"After {output}")
+#print(f"Avaliable elements : {my_list}")
+#for x in my_list: #in is the membership of list
+#    print(x)
+
+#using sort
+#print(str(my_list.sort()))
+
+#using slicing reverse the elements
+
+#print(my_list.clear()) #Returns None
+#output = my_list[::-1]
+#print(output)
+
+list_ex = [1,2,34,5,6,7,8,9]
+#list_ex.reverse()
+#print(list_ex)
+#list_ex.remove(34)
+#print(list_ex)
+#All methods list
+#append,pop,sort,slicing,reverse,len,in,count,clear,remove
+#You can change th values or elements in list
